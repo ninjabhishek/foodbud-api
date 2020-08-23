d452a77e0f6f230e846269b0a090e1cdbe6c4ab9
Abhishek Anand <anandabhishek58@gmail.com>
2020-08-23 23:48:01 +0530

    init

---
+++ src/main/java/org/ninjabhishek/foodbud/FoodBudApplication.java
@@ -0,0 +1,13 @@
+package org.ninjabhishek.foodbud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodBudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodBudApplication.class, args);
+    }
+
+}
